f092de26802112b6dc43769cfafee82d71cff2ef
crazyman9870 <crazyman9870@gmail.com>
2016-05-06 10:37:33 -0600

    Beginning of a database

---
+++ src/parser/DocumentLink.java
@@ -0,0 +1,10 @@
+package parser;
+
+public class DocumentLink {
+	
+	private int documentNumber;
+	private String documentPath;
+	private String documentInfo;
+	private int keywordFrequency;
+	
+}
--- src/parser/Tokenizer.java
+++ src/parser/Tokenizer.java
@@ -2,12 +2,14 @@ package parser;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.HashSet;
 import java.util.Scanner;
+import java.util.TreeMap;
 
 public class Tokenizer {
 	public static void main(String[] args) {
 		File filePath = new File("resources\\wikiFiles");
-		
+		TreeMap database = new TreeMap<String, HashSet>();
 		for(File f : filePath.listFiles()) {
 			try {
 				Scanner scan = new Scanner(f);
