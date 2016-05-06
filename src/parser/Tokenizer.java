eafd4438170f508f55097be374456c429db026d3
crazyman9870 <crazyman9870@gmail.com>
2016-05-05 22:42:52 -0600

    Creating classes

--- src/parser/Tokenizer.java
+++ src/parser/Tokenizer.java
@@ -1,11 +1,20 @@
 package parser;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class Tokenizer {
 	public static void main(String[] args) {
 		File filePath = new File("resources\\wikiFiles");
+		
 		for(File f : filePath.listFiles()) {
+			try {
+				Scanner scan = new Scanner(f);
+				
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			}
 			System.out.println(f.getAbsolutePath());
 		}
 	}
