5a28f92c7cd1c41c62b83656afa74ccda878f717
crazyman9870 <crazyman9870@gmail.com>
2016-05-07 14:38:00 -0600

    Working Data Storage

--- src/parser/DocumentLink.java
+++ src/parser/DocumentLink.java
@@ -1,10 +1,38 @@
 package parser;
 
-public class DocumentLink {
+public class DocumentLink implements Comparable<DocumentLink>{
 	
-	private int documentNumber;
-	private String documentPath;
-	private String documentInfo;
 	private int keywordFrequency;
+	private int fileEntryNumber;
 	
+	public DocumentLink() {
+		keywordFrequency = 1;
+		fileEntryNumber = -1;
+	}
+	
+	public DocumentLink(int counter) {
+		keywordFrequency = 1;
+		fileEntryNumber = counter;
+	}
+	
+	public int getKeywordFrequency() {
+		return keywordFrequency;
+	}
+
+	public void setKeywordFrequency(int keywordFrequency) {
+		this.keywordFrequency = keywordFrequency;
+	}
+	
+	public void incrementKeywordFrequency() {
+		this.keywordFrequency++;
+	}
+
+	//Sorts by keyword frequency then by whichever document was added first
+	@Override
+	public int compareTo(DocumentLink arg0) {
+		if((this.keywordFrequency - arg0.keywordFrequency) != 0)
+			return this.keywordFrequency - arg0.keywordFrequency;
+		else
+			return -(this.fileEntryNumber - arg0.fileEntryNumber);
+	}	
 }
--- src/parser/Tokenizer.java
+++ src/parser/Tokenizer.java
@@ -2,22 +2,24 @@ package parser;
 
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.HashSet;
-import java.util.Scanner;
+import java.util.Set;
 import java.util.StringTokenizer;
 import java.util.TreeMap;
+import java.util.TreeSet;
 
 public class Tokenizer {
+	
 	public static void main(String[] args) {
 		
 		StopWords stopWords = new StopWords();
 		//stopWords.printStopWords();
 		
 		File filePath = new File("resources\\wikiFiles");
-		TreeMap database = new TreeMap<String, HashSet<DocumentLink>>();
+		TreeMap<String, TreeMap<String,DocumentLink>> database = new TreeMap<>();
+		int counter = 1;
 		for(File f : filePath.listFiles()) {
 			try {
 				BufferedReader in = new BufferedReader(new FileReader(f));
@@ -31,14 +33,34 @@ public class Tokenizer {
 				while(st.hasMoreTokens()){
 					nextLine = st.nextToken();
 					if(nextLine.matches("[a-zA-Z'.]*") && !stopWords.contains(nextLine)) {
-						System.out.println(nextLine);
+						/* String Formatting */
+						nextLine = nextLine.toLowerCase();
+						if(nextLine.contains("."))
+							nextLine = (String) nextLine.subSequence(0, nextLine.length()-1);
+						if(nextLine.contains("'s"))
+							nextLine = (String) nextLine.substring(0, nextLine.length()-2);
+						/* Check if the keyword already exists */
+						if(database.containsKey(nextLine)) {
+							if(database.get(nextLine).containsKey(f.getPath()))
+								database.get(nextLine).get(f.getPath()).incrementKeywordFrequency();
+							else {
+								database.get(nextLine).put(f.getPath(), new DocumentLink(counter));
+							}
+						}
+						else {
+							TreeMap<String, DocumentLink> newMap = new TreeMap<>();
+							newMap.put(f.getPath(), new DocumentLink(counter));
+							database.put(nextLine, newMap);
+						}
+						//System.out.println(nextLine);
 					}
 				}
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
-			System.out.println(f.getAbsolutePath());
-			System.out.println("STOP");
+			//System.out.println(f.getPath());
+			counter++;
 		}
+		System.out.println("DONE");
 	}
 }
