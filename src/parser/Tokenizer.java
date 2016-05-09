e241166ca9cef791c963a8e28c79f3c7db5e47b9
crazyman9870 <crazyman9870@gmail.com>
2016-05-09 05:14:19 -0600

    As far as I am getting

--- src/parser/Tokenizer.java
+++ src/parser/Tokenizer.java
@@ -2,6 +2,7 @@ package parser;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.HashSet;
@@ -41,22 +42,22 @@ public class Tokenizer {
 					nextLine = st.nextToken();
 					if(nextLine.matches("[a-zA-Z'.]*") && !stopWords.contains(nextLine)) {
 						/* String Formatting */
-						nextLine = nextLine.toLowerCase();
-						if(nextLine.contains(".") || nextLine.contains(","))
-							nextLine = (String) nextLine.subSequence(0, nextLine.length()-1);
-						if(nextLine.contains("'s"))
-							nextLine = (String) nextLine.substring(0, nextLine.length()-2);
-						if(nextLine.contains("'") && nextLine.length() > 1) {
-//							System.out.println(nextLine);
-							if(nextLine.charAt(0) == '\'')
-								nextLine = (String) nextLine.substring(1, nextLine.length());
-							if(nextLine.charAt(0) == '\'')
-								nextLine = (String) nextLine.substring(1, nextLine.length());
-							if(nextLine.charAt(nextLine.length()-1) == '\'')
-								nextLine = (String) nextLine.substring(0, nextLine.length()-1);
-//							System.out.println(nextLine);
-						}
-						//nextLine = stemmer.stem(nextLine);
+//						nextLine = nextLine.toLowerCase();
+//						if(nextLine.contains(".") || nextLine.contains(","))
+//							nextLine = (String) nextLine.subSequence(0, nextLine.length()-1);
+//						if(nextLine.contains("'s"))
+//							nextLine = (String) nextLine.substring(0, nextLine.length()-2);
+//						if(nextLine.contains("'") && nextLine.length() > 1) {
+////							System.out.println(nextLine);
+//							if(nextLine.charAt(0) == '\'')
+//								nextLine = (String) nextLine.substring(1, nextLine.length());
+//							if(nextLine.charAt(0) == '\'')
+//								nextLine = (String) nextLine.substring(1, nextLine.length());
+//							if(nextLine.charAt(nextLine.length()-1) == '\'')
+//								nextLine = (String) nextLine.substring(0, nextLine.length()-1);
+////							System.out.println(nextLine);
+//						}
+						nextLine = stemmer.stem(nextLine.toLowerCase());
 						/* Check if the keyword already exists */
 						if(database.containsKey(nextLine)) {
 							if(database.get(nextLine).containsKey(f.getPath()))
@@ -70,6 +71,7 @@ public class Tokenizer {
 							newMap.put(f.getPath(), new DocumentLink(wordCount, counter));
 							database.put(nextLine, newMap);
 						}
+						
 						//System.out.println(nextLine);
 					}
 				}
@@ -90,7 +92,7 @@ public class Tokenizer {
 			StringTokenizer query = new StringTokenizer(scan.nextLine());//create a string
 			while(query.hasMoreTokens()) {
 //				System.out.println("STARTING WHILE");
-				String word = query.nextToken();
+				String word = stemmer.stem(query.nextToken());
 				if(word.equals("quit")) {
 					scan.close();
 					return;
@@ -98,10 +100,40 @@ public class Tokenizer {
 				System.out.println(word);
 				TreeMap<String, DocumentLink> docs = database.get(word);
 				for(Entry<String, DocumentLink> entry : docs.entrySet()) {
+					
+					
+					//Not Sure
+					int maxFrequency = 0;
+					BufferedReader buff;
+					try {
+						buff = new BufferedReader(new FileReader(entry.getKey()));
+						StringBuffer strbuff = new StringBuffer();
+						String testingStr = "";
+						while ((testingStr = buff.readLine()) != null)
+							strbuff.append(testingStr+"\n");
+						buff.close();
+						//save it to a bin tree.
+						StringTokenizer token = new StringTokenizer(strbuff.toString());//create a string
+						while(token.hasMoreTokens()){
+							testingStr = token.nextToken();
+							if(testingStr.matches("[a-zA-Z'.]*") && !stopWords.contains(testingStr)) {
+								testingStr = stemmer.stem(testingStr.toLowerCase());
+								int testingFrequency = database.get(testingStr).get(entry.getKey()).getKeywordFrequency();
+								if(testingFrequency > maxFrequency)
+									maxFrequency = testingFrequency;
+							}
+						}
+					} catch (Exception e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					//Not Sure			
+					
+					
 					if(scores.containsKey(entry.getKey()))
-						scores.updateScore(entry.getKey(), calculateScore(word, entry.getValue(), counter, docs.size()));
+						scores.updateScore(entry.getKey(), calculateScore(word, entry.getValue(), counter, docs.size(), maxFrequency));
 					else
-						scores.addNode(entry.getKey(), calculateScore(word, entry.getValue(), counter, docs.size()));
+						scores.addNode(entry.getKey(), calculateScore(word, entry.getValue(), counter, docs.size(), maxFrequency));
 				}
 				//System.out.println("OUT OF FOR");
 				System.out.println(scores.getSize());
@@ -111,11 +143,12 @@ public class Tokenizer {
 		}
 	}
 	
-	private static double calculateScore(String word, DocumentLink doc, int docTotal, int keywordDocTotal) {
-		double termFrequency = ((double)doc.getKeywordFrequency()/(double)doc.getWordCount());
+	private static double calculateScore(String word, DocumentLink doc, int docTotal, int keywordDocTotal, int maxFrquency) {
+//		double termFrequency = ((double)doc.getKeywordFrequency()/(double)doc.getWordCount());
+//		double termFrequency = ((double)doc.getKeywordFrequency()/(double)maxFrquency);
 		double inverseDocumentFrequency = (Math.log(((double) docTotal / (double) keywordDocTotal))/Math.log(2.0));
-		System.out.println(inverseDocumentFrequency);
-		double queryScore = (termFrequency * inverseDocumentFrequency);
+//		double queryScore = (termFrequency * inverseDocumentFrequency);
+		double queryScore = ((double)doc.getKeywordFrequency() * inverseDocumentFrequency);
 		return queryScore;
 	}
 	
